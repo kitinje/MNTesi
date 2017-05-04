9d7238c2bc9d41cdf287f448178eb171d9c7e11c
kitinje <marco.bolis@marionegri.it>
2017-05-04 10:40:38 +0200

    Create analisi.R

---
+++ RNASeq_R/analisi.R
@@ -0,0 +1,5 @@
+# TAB files in /pico/scratch/userexternal/mfratell/MARTINA/RNASeq/TAB
+# scp mfratell@login.pico.cineca.it:/pico/scratch/userexternal/mfratell/MARTINA/RNASeq/TAB/*.tab ./
+
+setwd("/Users/age/Desktop/NUOVE/")
+
