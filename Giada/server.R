b8525ee8c89b5aafcb2474635b054fc32e8e4fdc
kitinje <marco.bolis@marionegri.it>
2017-09-27 17:42:09 +0200

    Create server.R

---
+++ Giada/server.R
@@ -0,0 +1,11 @@
+
+qsub -q parallel -I -l select=1:ncpus=20:mem=120G,walltime=12:00:00 -A ELIX2_prj9
+cd /pico/scratch/userexternal/mfratell/GIADA/ageing
+module load autoload r
+R
+
+quit()
+exit #uscire dal job
+
+
+scp mfratell@login.pico.cineca.it:/pico/scratch/userexternal/mfratell/GIADA/ageing/WKSpace27.RData ./
